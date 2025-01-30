b14abea5483f3724980f46556377c60b076f6882
awangran <ashleero.yin@gmail.com>
2025-01-30 16:34:57 -0500

    cambios del Estudiante 1

--- App/view.py
+++ App/view.py
@@ -55,9 +55,9 @@ def print_menu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
-    # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
     # Agregue la opción 3 para cargar los tags de los libros.
     # Pueede guiarse de las opciones 1 y 2.
+    print("3- Cargar Tags de Libros")
     print("0- Salir")
 
 
@@ -95,16 +95,16 @@ def load_books_tags(app):
     :param app: Aplicación de la lógica
     :type app: logic
     """
-    # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    pass
+    book_tags = logic.load_books_tags(app,"GoodReads/book_tags-small.csv")
+    return book_tags
 
 
 def first_book(app):
     """
     Devuelve el primer libro cargado en el conjunto de libros
     """
-    # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    pass
+    first = logic.first_book(app)
+    return first
 
 
 def last_book(app):
@@ -137,9 +137,9 @@ def main():
             books = load_books(app)
             print("Total de libros cargados: " + str(books) + "\n")
 
-            # TODO: Mods de Est-1 en el Lab 2
-            first = None
-
+            first = first_book(app)
+            print("Primer libro cargado:\n" + str(first) + "\n")
+            
             # TODO: Mods de Est-2 en el Lab 2
             last = None
 
